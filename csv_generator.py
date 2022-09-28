05e8d55bcbe40d2c53f691d6b1116d5880fc6d2f
ThePizar <joshi.michel@gmail.com>
2022-09-28 15:21:49 -0400

    Change to TSV and add WGID

--- csv_generator.py
+++ csv_generator.py
@@ -5,7 +5,7 @@ reward_tank_ids = [58369, 46849, 15617, 57937]
 meta_tank_ids = [6193, 16897, 22017, 15697, 19009, 2929, 5265]
 situational_tank_ids = [3681, 14609, 4737, 9489, 3473, 13857, 13089]
 
-with open("2021-11-19.json", 'r') as json_file:
+with open("2022-09-28.json", 'r') as json_file:
     player_data = json.load(json_file)
 
 total_marks = []
@@ -23,6 +23,7 @@ for member in player_data:
             else:
                 print('fuck')
             tank_player_dict['name'] = member['summary']['nickname']
+            tank_player_dict['wgid'] = member['summary']['account_id']
             tank_player_dict['tank'] = tank['name']
             tank_player_dict['mark_value'] = tank['moe']
             tank_player_dict['position'] = member['clanData']['role']
@@ -30,12 +31,12 @@ for member in player_data:
         if count !=0:
             total_marks.append(tank_player_dict)
 
-csv_columns = ['name', 'tank', 'mark_value', 'type', 'position']
-csv_file = 'raw.csv'
+csv_columns = ['name', 'wgid', 'tank', 'mark_value', 'type', 'position']
+csv_file = 'raw.tsv'
 
 try:
-    with open(csv_file, "w") as csvFile:
-        writer = csv.DictWriter(csvFile, fieldnames=csv_columns)
+    with open(csv_file, "w", newline='') as csvFile:
+        writer = csv.DictWriter(csvFile, fieldnames=csv_columns, delimiter='\t')
         writer.writeheader()
         for data in total_marks:
             writer.writerow(data)
